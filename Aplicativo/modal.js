8ca561ff73c1fa2f5c36e214510273fbad4602fa
Pedro Kauã <pk.bleite@gmail.com>
2022-11-24 06:25:21 -0300

    modalDone

--- Aplicativo/modal.js
+++ Aplicativo/modal.js
@@ -1,1 +1,16 @@
 const addTaskButton = document.querySelector("#plusIconButton");
+const modal = document.getElementById("modal");
+const closeButton = document.querySelector("#closeButton");
+
+let showModal = function()  {
+    addTaskButton.addEventListener("click", () =>{
+        modal.style.display = "flex";
+    })
+    return checkCloseButton();
+}
+let checkCloseButton = function()   {
+    closeButton.addEventListener("click", () =>{
+        modal.style.display = "none";
+    })
+}
+showModal();
