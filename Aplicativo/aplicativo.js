49692225454f127f2bb6b7827f2daf4ff01947d4
Pedro Kauã <pk.bleite@gmail.com>
2022-11-23 22:42:03 -0300

    startingLogic

---
+++ Aplicativo/aplicativo.js
@@ -0,0 +1,4 @@
+const homeFolder = document.getElementById("textHome");
+const toggleFolder = document.getElementById("textFolder");
+const priorityButtton = document.querySelector("#priorityButton");
+const deleteButton = document.querySelector("#deleteButton");
